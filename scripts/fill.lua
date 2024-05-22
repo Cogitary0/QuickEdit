97d111659c6945b7067cf308c2b0683fdc04c182
Cogitary <84321459+DoubleDataStack@users.noreply.github.com>
2024-05-22 21:46:40 +0300

    Update fill.lua

--- scripts/fill.lua
+++ scripts/fill.lua
@@ -3,13 +3,10 @@ local edt = require("quickedit:editor")
 local click_fill = 0
 local start_pos = {0, 0, 0}
 
-
-
 function on_broken(x, y, z)
     click_fill = 0
     start_pos = {0, 0, 0}
 end
-
 
 function on_placed(x, y, z)
     if click_fill == 0 then
