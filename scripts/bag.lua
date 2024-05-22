9a524247dc5167dc0f1db7a3d784335aeef4907e
Xertis <xertis3785@gmail.com>
2024-05-22 23:29:20 +0300

    the start of the rework painter
    1) Добавлен мешок, в который собираются блоки, которые будут использоваться при постройке
    2) Добавлен пока бесполезный терраформер
    3) Переписал код editor на таблицы для работы с мешком
    4) Переписал скрипт fill и replace на мешки, остальные мешки просто используют старую систему
    5) Удалил комент из json

---
+++ scripts/bag.lua
@@ -0,0 +1,12 @@
+local container = require 'quickedit:container'
+local bag_inventory = {}
+
+function on_use_on_block(x, y, z, playerid)
+    bag_inventory[#bag_inventory + 1] = get_block(x, y, z)
+    container:send_bag(bag_inventory)
+end
+
+function on_block_break_by(x, y, z, playerid)
+    bag_inventory = {}
+    container:send_bag({})
+end
--- scripts/cylinder.lua
+++ scripts/cylinder.lua
@@ -12,7 +12,7 @@ function on_placed(x, y, z)
 		editor.cylinder(
 			start_pos,
 			{x, y, z},
-			get_block(x, y - 1, z)
+			{get_block(x, y - 1, z)}
 		)
 		clck, start_pos = 0, {0, 0, 0}
 	end
