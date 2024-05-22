cf607bc3564e37b2d3fdbcb56b7f968fc2a1b170
Cogitary <84321459+DoubleDataStack@users.noreply.github.com>
2024-05-22 21:47:06 +0300

    Update cuboid.lua

--- scripts/cuboid.lua
+++ scripts/cuboid.lua
@@ -3,12 +3,10 @@ local edt = require("quickedit:editor")
 local click_create_cuboid = 0
 local start_pos_create = {0, 0, 0}
 
-
 function on_broken(x,y,z)
     click_create_cuboid = 0
     start_pos_create = {0, 0, 0}
 end
-
 
 function on_placed(x, y, z)
     if click_create_cuboid == 0 then 
